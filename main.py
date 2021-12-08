bf28ba9325c635a46d5594e7c7363abc70ec3f59
Benjamin Chinwe <chisoftltd@gmail.com>
2021-12-08 14:49:40 +0000

    Format account into: name, description and country

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ def get_account():
 	"""Get any account from data"""
 	return random.choice(data)
 
+def account_format(account):
+	"""Format account into: name, description and country"""
+	name = account["name"]
+	description = account["description"]
+	country = account["country"]
+	return f"{name}, a {description}, from {country}"
+
 print(logo)
 
 
